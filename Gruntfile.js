4fb8b4be58020ab5665aa75c7bcae8e11bfd0eb8
Sangmin Yoo <sangmin.yoo@iamota.com>
2014-06-10 13:20:38 -0700

    will get an error

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,31 @@
 module.exports = function(grunt) {
- 
-  grunt.initConfig({
-    jshint: {
-      all: ['Gruntfile.js', 'hello.js']
+
+// load all grunt tasks
+require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+grunt.initConfig({
+	jshint: {
+    	all: ['Gruntfile.js', 'hello.js']
+    },
+    compass: {                  // Task
+    dist: {                   // Target
+      options: {              // Target options
+        sassDir: 'sass',
+        cssDir: 'css',
+        environment: 'production'
+      }
+    },
+    dev: {                    // Another target
+      options: {
+        sassDir: 'sass',
+        cssDir: 'css'
+      }
     }
-  });
- 
+  }
+});
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', 'jshint');
- 
+  grunt.registerTask('default', ['jshint', 'compass']);
+
+
 };
